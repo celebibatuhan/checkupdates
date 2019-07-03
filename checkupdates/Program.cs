a48368848fccf3fa1c24af7ff79049b08e91c1f2
Batuhan Çelebi <batuhan.celebi@yemeksepeti.com>
2019-07-03 17:21:52 +0300

    Add project files.

---
+++ checkupdates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkupdates
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher(@"C:\Users\batuhan.celebi\Informations");
+
+            watcher.EnableRaisingEvents = true;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Changed += watcher_Changed;
+            watcher.Created += watcher_Created;
+            watcher.Renamed += watcher_Renamed;
+
+            Console.Read();
+            
+
+
+
+        }
+
+        private static void watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine("File : {0} renamed to {1} at time : {2}", e.OldName, e.Name, DateTime.Now.ToLocalTime());
+        }
+
+        private static void watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File : {0}  created at time : {1}", e.Name, DateTime.Now.ToLocalTime());
+        }
+
+        private static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File : {0} changed at time : {1}", e.Name, DateTime.Now.ToLocalTime());
+        }
+    }
+}
